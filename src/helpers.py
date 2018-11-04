2e6a48d7a3917648f5cf8ec4e99373113c6b9c5d
DreamingRaven <GeorgeRavenNR@outlook.com>
2018-11-04 18:33:49 +0000

    Added basic frame for serverus

---
+++ src/helpers.py
@@ -0,0 +1,10 @@
+# @Author: George Onoufriou <georgeraven>
+# @Date:   2018-11-04
+# @Filename: helpers.py
+# @Last modified by:   georgeraven
+# @Last modified time: 2018-11-04
+# @License: Please see LICENSE in project root.
+# @Copyright: George Onoufriou
+
+def placeholder():
+    print("placeholder")
